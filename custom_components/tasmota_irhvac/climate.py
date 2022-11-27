4ec18a93be4e865fdfbae1d4801031dd657ed3ff
PostLogical <robby.griffin@gmail.com>
2022-11-26 19:03:41 -0500

    Try retaining mqtt

--- custom_components/tasmota_irhvac/climate.py
+++ custom_components/tasmota_irhvac/climate.py
@@ -953,6 +953,8 @@ class TasmotaIrhvac(RestoreEntity, ClimateEntity):
 
         return unsubscribe
 
+        return unsubscribe
+
     async def async_will_remove_from_hass(self):
         """Unsubscribe when removed."""
         for unsubscribe in self._unsubscribes:
