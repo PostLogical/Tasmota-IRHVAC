79c37fc3828babb66018ea4a38440edbb4c36abf
PostLogical <24210791+PostLogical@users.noreply.github.com>
2025-01-04 08:12:43 +0000

    Removed await from async_set_preset_mode update_ha_state section.

--- custom_components/tasmota_irhvac/climate.py
+++ custom_components/tasmota_irhvac/climate.py
@@ -1315,7 +1315,7 @@ class TasmotaIrhvac(RestoreEntity, ClimateEntity):
             path = self.topic.split('/')
             irsend = "cmnd/" + path[1] + "/irsend"
             await mqtt.async_publish(self.hass, irsend, payload_data)
-            return await self.async_schedule_update_ha_state()
+            return self.async_schedule_update_ha_state()
             """Test this above section and finish the rest if it works"""
         await self.send_ir()
 
